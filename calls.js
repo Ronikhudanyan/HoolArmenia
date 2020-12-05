726c478d28a12f30da694fa29a660a0f8d05620f
Ronikhudanyan <Ronikhudanyan@gmail.com>
2020-12-04 16:46:57 -0800

    final edit for config

--- calls.js
+++ calls.js
@@ -31,7 +31,7 @@
 
 
 const Sequelize = require('sequelize');
-const dBase = require('./config/config.json');
+const dBase = require('./config/database.js');
 
 const calls = dBase.define('call', {
   title: {
